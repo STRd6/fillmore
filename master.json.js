8fe0d51681542303b8955e1b44ba1210f29a299b
Daniel X Moore <yahivin@gmail.com>
2015-09-08 11:58:14 -0700

    :joy::gem: Updated in browser at strd6.github.io/editor

---
+++ master.json.js
@@ -0,0 +1,48 @@
+window["STRd6/fillmore:master"]({
+  "source": {
+    "LICENSE": {
+      "path": "LICENSE",
+      "content": "The MIT License (MIT)\n\nCopyright (c) 2015 Daniel X Moore\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the \"Software\"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.\n\n",
+      "mode": "100644",
+      "type": "blob"
+    },
+    "README.md": {
+      "path": "README.md",
+      "content": "# fillmore\nAn operating system written in Fillmore, CA\n",
+      "mode": "100644",
+      "type": "blob"
+    },
+    "main.coffee": {
+      "path": "main.coffee",
+      "content": "# Probably want a bunch of garish widgets... pop-out, drag to move\n\n# Need to be able to launch the editor and create new applications/widgets\n\n# Viewers for various media types\n\n# It should be FUN!\n\n# Sample Use Case\n# Launch Editor\n# Create Widget\n# Save Widget\n# Launch Widget\n\nalert 'heyy'\n",
+      "mode": "100644"
+    },
+    "templates/widget.jade": {
+      "path": "templates/widget.jade",
+      "content": ".widget\n  h2 Title\n  iframe\n",
+      "mode": "100644"
+    }
+  },
+  "distribution": {
+    "main": {
+      "path": "main",
+      "content": "(function() {\n  alert('heyy');\n\n}).call(this);\n",
+      "type": "blob"
+    }
+  },
+  "progenitor": {
+    "url": "http://www.danielx.net/editor/"
+  },
+  "entryPoint": "main",
+  "repository": {
+    "branch": "master",
+    "default_branch": "master",
+    "full_name": "STRd6/fillmore",
+    "homepage": null,
+    "description": "An operating system written in Fillmore, CA",
+    "html_url": "https://github.com/STRd6/fillmore",
+    "url": "https://api.github.com/repos/STRd6/fillmore",
+    "publishBranch": "gh-pages"
+  },
+  "dependencies": {}
+});
